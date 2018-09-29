0269cd6d7a074505a93c8a33c01666b11c325a25
SATEJ SARKER <satejall@gmail.com>
2018-09-29 22:31:15 +0600

    firebase store config start

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {createStore,combineReducers,compose} from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase,firebaseReducer } from 'react-redux-firebase';
+import {reduxFirestore, firestoreReducer } from 'redux-firestore';
+//todo 
+
+//custom reducer 
+
+
+const firebaseConfig = {};
